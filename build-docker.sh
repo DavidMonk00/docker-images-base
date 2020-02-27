8feac4027b0695cdb9efd6d753fd45d893413b70
David Gabriel Monk <david.gabriel.monk@cern.ch>
2020-02-27 18:00:50 +0000

    Update build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,6 @@
-docker build ./aarch64-centos8 -t dmonk/aarch64-centos8
-docker build ./aarch64-centos8-boost -t dmonk/aarch64-centos8-boost
-docker build ./aarch64-centos8-uhal -t dmonk/aarch64-centos8-uhal
+docker pull dmonk/aarch64-centos8
+docker build ./aarch64-centos8 -t dmonk/aarch64-centos8 && docker push dmonk/aarch64-centos8
+docker pull dmonk/aarch64-centos8-boost
+docker build ./aarch64-centos8-boost -t dmonk/aarch64-centos8-boost && docker push dmonk/aarch64-centos8-boost
+docker pull dmonk/aarch64-centos8-uhal
+docker build ./aarch64-centos8-uhal -t dmonk/aarch64-centos8-uhal && docker push dmonk/aarch64-centos8-uhal
