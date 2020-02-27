0a728d72d25a94f31d50d76f9526ad3d160a0313
David Gabriel Monk <david.gabriel.monk@cern.ch>
2020-02-27 17:57:50 +0000

    Update build-docker.sh

---
+++ build-docker.sh
@@ -0,0 +1,3 @@
+docker build ./aarch64-centos8 -t dmonk/aarch64-centos8
+docker build ./aarch64-centos8-boost -t dmonk/aarch64-centos8-boost
+docker build ./aarch64-centos8-uhal -t dmonk/aarch64-centos8-uhal
