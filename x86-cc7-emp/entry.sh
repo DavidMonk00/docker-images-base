becc2eb7007f6be7613bfccca2096319c336c138
David Monk <d.g.monk0@gmail.com>
2020-03-23 17:10:53 +0100

    Added cc7 emp

---
+++ x86-cc7-emp/entry.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# . /emp-toolbox/env.sh
+export PATH=/empbutler-cpp/bin:$PATH
+export LD_LIBRARY_PATH=/opt/cactus/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/emp-toolbox/core/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/emp-toolbox/logger/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/empbutler-cpp/lib:$LD_LIBRARY_PATH
+exec "$@"
+echo "Hello world!"
