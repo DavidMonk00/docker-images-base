c5d71344c0a922cddf69f2cbf4d4ed2530af5761
David Monk <d.g.monk0@gmail.com>
2020-03-30 17:44:31 +0100

    Pre shuffle commit

--- x86-cc7-emp/entry.sh
+++ x86-cc7-emp/entry.sh
@@ -5,4 +5,18 @@ export LD_LIBRARY_PATH=/opt/cactus/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/emp-toolbox/core/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/emp-toolbox/logger/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/empbutler-cpp/lib:$LD_LIBRARY_PATH
+
+export LD_LIBRARY_PATH=/opt/smash/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/serenity-toolbox/core/lib:$LD_LIBRARY_PATH
+
+export PYTHONPATH=/serenity-toolbox/python/pkg:$PYTHONPATH
+export PATH=/serenity-toolbox/python/scripts:$PATH
+
+export PATH
+export LD_LIBRARY_PATH
+export PYTHONPATH
+
+export SMASH_PATH=/opt/smash/
+
+
 exec "$@"
